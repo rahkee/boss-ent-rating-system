44d24c65128c94d99f0c3b06787ce44252ffc31d
Ray Villaraza <rvillaraza9362@fs1477mbpro.avaflipswitch.com>
2018-04-25 15:05:48 -0700

    yes! Some headway

--- js/be-scheduler.js
+++ js/be-scheduler.js
@@ -1,23 +1,63 @@
-document.addEventListener("DOMContentLoaded", function(){
+"use strict";
 
-});
+var nameInput = document.getElementById('inputName');
+var count = 0;
+var bossBae = [];
 
-/***** Adding People to the List *****/
-    // Initialize INPUT field
-    // List to LOOP through array
-    // LOOP through array and DISPLAY values
-    // INPUT field value to add to the array
-    // Save
+function createBossBae() {
 
-/***** Removing People from the List *****/
-    // Remove Person from array
-    // LOOP through array and DISPLAY new values
-    // Save
+    var name = nameInput.value;
 
-/***** Rating People *****/
-    // Initialize Up and Down Buttons
-    // Set Ratings, Update Object
-    // Save
+    if (!duplicateExists(name) && inputName.value !== "") {
 
-/***** Organize List *****/
-    // List highest rating first
+        addBaeToList(name);
+        inputName.value = "";
+    }
+
+    showBossBaes();
+}
+
+var baeList = document.getElementById('nameList');
+
+function showBossBaes() {
+
+    while (baeList.firstChild) {
+        baeList.removeChild(baeList.firstChild)
+    }
+
+    for (var bae of bossBae) {
+        var baeListNode = document.createElement('li');
+        var baeNameText = document.createTextNode(bae.name);
+        baeListNode.appendChild(baeNameText);
+        baeList.appendChild(baeListNode);
+    }
+}
+
+function duplicateExists(name) {
+
+    for (var bae of bossBae) {
+
+        if (name === bae.name) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function addBaeToList(name) {
+
+    bossBae.push({
+        id: count + 1,
+        name: name,
+        rating: 0
+    })
+
+    count++;
+}
+
+inputName.addEventListener("keyup", function(e) {
+    if (e.keyCode === 13) {
+        createBossBae()
+    }
+}, false);
